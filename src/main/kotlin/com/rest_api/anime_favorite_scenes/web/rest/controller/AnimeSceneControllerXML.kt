8b7b0fae7897af19047c0c6deaee4bd48b5d182b
Kasoma <kaua.maia@sienacompany.com>
2024-08-05 10:04:22 -0300

    [Fix] Permitir CORS

--- src/main/kotlin/com/rest_api/anime_favorite_scenes/web/rest/controller/AnimeSceneControllerXML.kt
+++ src/main/kotlin/com/rest_api/anime_favorite_scenes/web/rest/controller/AnimeSceneControllerXML.kt
@@ -7,16 +7,9 @@ import com.rest_api.anime_favorite_scenes.repository.SceneRepository
 import com.rest_api.anime_favorite_scenes.service.local.SceneService
 import com.rest_api.anime_favorite_scenes.service.remote.TraceMoeConnection
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/xml/scenes", produces = [MediaType.APPLICATION_XML_VALUE])
 class AnimeSceneControllerXML(
